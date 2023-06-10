b48913496904dcdb6a6528fe2dfa692603e62979
Lucas Jdev <lucas.jdev1@gmail.com>
2023-06-10 11:44:39 -0300

    return

--- src/main/java/br/com/logiquesistemas/task/domain/error/ParamInvalid.java
+++ src/main/java/br/com/logiquesistemas/task/domain/error/ParamInvalid.java
@@ -1,10 +1,8 @@
 package br.com.logiquesistemas.task.domain.error;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseBody
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class ParamInvalid extends IllegalArgumentException{
     
