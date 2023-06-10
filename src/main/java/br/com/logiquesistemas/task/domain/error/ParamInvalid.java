ec6b5d77a72335657bf3adff2ef7ec97407c3c5b
Lucas Jdev <lucas.jdev1@gmail.com>
2023-06-10 11:36:45 -0300

    create error param invalid

---
+++ src/main/java/br/com/logiquesistemas/task/domain/error/ParamInvalid.java
@@ -0,0 +1,17 @@
+package br.com.logiquesistemas.task.domain.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class ParamInvalid extends IllegalArgumentException{
+    
+    public ParamInvalid() {
+        super("Parâmetro inválido!");
+    }
+
+    public ParamInvalid(String message) {
+        super(message);
+    }
+
+}
