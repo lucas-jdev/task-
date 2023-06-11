4f415c929ea99d60d03672b4bb79574f75454756
Lucas Jdev <lucas.jdev1@gmail.com>
2023-06-11 11:12:56 -0300

    modify: param invalid exception implemented

--- src/test/java/br/com/logiquesistemas/task/integration/service/UpdateTaskTest.java
+++ src/test/java/br/com/logiquesistemas/task/integration/service/UpdateTaskTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import br.com.logiquesistemas.task.TaskApplication;
 import br.com.logiquesistemas.task.domain.entities.StatusTask;
 import br.com.logiquesistemas.task.domain.entities.Task;
+import br.com.logiquesistemas.task.domain.error.ParamInvalid;
 import br.com.logiquesistemas.task.domain.repository.TaskRepo;
 import br.com.logiquesistemas.task.service.UpdateTask;
 import br.com.logiquesistemas.task.service.UpdateTask.InnerTaskServiceDTO;
@@ -31,7 +32,7 @@ class UpdateTaskTest {
     private Task task;
 
     @BeforeAll
-    void setup() {
+    void setup() throws ParamInvalid {
         task = new Task(TITLE, DESCRIPTION);
         repo.create(task);
     }
@@ -43,7 +44,7 @@ class UpdateTaskTest {
 
     @Test
     @DisplayName("Deve atualizar uma task no banco")
-    void testUpdateTaskInDB() {
+    void testUpdateTaskInDB() throws ParamInvalid {
         var updateTask = new UpdateTask(repo);
 
         task.changeStatus(StatusTask.IN_PROGRESS.toString());
