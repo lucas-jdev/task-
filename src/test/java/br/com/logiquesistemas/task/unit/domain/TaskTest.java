d0c379174aeeeb8e18ef9e9b9bd00183ae523b8e
Lucas Jdev <lucas.jdev1@gmail.com>
2023-06-11 11:11:26 -0300

    modify task test

--- src/test/java/br/com/logiquesistemas/task/unit/domain/TaskTest.java
+++ src/test/java/br/com/logiquesistemas/task/unit/domain/TaskTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import br.com.logiquesistemas.task.domain.entities.StatusTask;
 import br.com.logiquesistemas.task.domain.entities.Task;
+import br.com.logiquesistemas.task.domain.error.ParamInvalid;
 
 @DisplayName("Testes de unidade da classe Task")
 class TaskTest {
@@ -23,10 +24,12 @@ class TaskTest {
 
     private final String DESCRIPTION_INVALID = null;
 
+    private Task task;
+
     @Test
     @DisplayName("Deve criar Task com argumentos")
-    void testCreateTaskWithArgs() {
-        Task task = new Task(TITLE, DESCRIPTION);
+    void testCreateTaskWithArgs() throws ParamInvalid {
+        task = new Task(TITLE, DESCRIPTION);
         assertEquals(TITLE, task.title());
         assertEquals(DESCRIPTION, task.description());
         assertEquals(STATUS_TASK, task.status());
@@ -45,28 +48,32 @@ class TaskTest {
         @DisplayName("Deve lançar exceção de argumento inválido para título inválido")
         void titleInvalidShouldToReturnException(String title) {
             String titleInvalid = checkIfNullIsText(title);
-            assertThrows(IllegalArgumentException.class, () -> new Task(titleInvalid, DESCRIPTION));
+            assertThrows(ParamInvalid.class, () -> new Task(titleInvalid, DESCRIPTION));
         }
 
         @Test
         @DisplayName("Deve lançar exceção de argumento inválido para descrição inválida")
         void descriptionInvalidShouldToReturnException() {
-            assertThrows(IllegalArgumentException.class, () -> new Task(TITLE, DESCRIPTION_INVALID));
+            assertThrows(ParamInvalid.class, () -> new Task(TITLE, DESCRIPTION_INVALID));
         }
 
         @Test
         @DisplayName("Deve lançar exceção de argumento inválido para status null")
         void changeStatusNullShouldToReturnException() {
-            Task task = new Task(TITLE, DESCRIPTION);
-            assertThrows(IllegalArgumentException.class, () -> task.changeStatus(null));
+            assertThrows(ParamInvalid.class, () -> {
+                task = new Task(TITLE, DESCRIPTION);
+                task.changeStatus(null);
+            });
         }
 
         @Test
         @DisplayName("Deve lançar exceção de argumento inválido para status inválido quando atributo já estiver COMPLETED")
         void changeStatusInvalidWhenAttributeIsCOMPLETED() {
-            Task task = new Task(TITLE, DESCRIPTION);
-            task.changeStatus(StatusTask.COMPLETED.toString());
-            assertThrows(IllegalArgumentException.class, () ->  task.changeStatus(StatusTask.IN_PROGRESS.toString()));
+            assertThrows(ParamInvalid.class, () ->  {
+                task = new Task(TITLE, DESCRIPTION);
+                task.changeStatus(StatusTask.COMPLETED.toString());
+                task.changeStatus(StatusTask.IN_PROGRESS.toString());
+            });
         }
 
         private String checkIfNullIsText(String value) {
